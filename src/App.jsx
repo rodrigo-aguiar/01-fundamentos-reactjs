95b711ca97774c482a71a923e9c82cda1e9482bd
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-23 22:06:38 -0300

    Iniciando com React - Propriedades
    Outro conceito super importante do React são as propriedades.
    Nessa aula, vamos entender o que são as propriedades e começar a utilizá-las nos nossos componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="John Doe"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias fugiat odio dolores consequuntur cupiditate recusandae ipsa necessitatibus fugit hic vero, asperiores quidem placeat error quam sequi, qui veniam rem. Ducimus."
+      />
+      <Post
+        author="John Doe 2"
+        content="A new nice post."
+      />
     </div>
   )
 }
