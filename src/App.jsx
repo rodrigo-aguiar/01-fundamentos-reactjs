e9b8954ae1a36549a4696e09e437a5b8c57fea1b
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-25 18:49:26 -0300

    Estrutura da aplicação - CSS Modules
    Nessa aula a gente vai conhecer a aplicação que vamos desenvolver durante esse módulo e começar com a estilização dela.
    Para isso, vamos ver como utilizar os CSS Modules, suas diferenças do CSS tradicional e como ele pode nos beneficiar durante o desenvolvimento dos nossos apps.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="John Doe"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias fugiat odio dolores consequuntur cupiditate recusandae ipsa necessitatibus fugit hic vero, asperiores quidem placeat error quam sequi, qui veniam rem. Ducimus."
