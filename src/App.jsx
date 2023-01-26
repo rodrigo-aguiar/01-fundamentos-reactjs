43e38f354b948c92c970553a0efbce41bfd5dce5
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-25 21:03:26 -0300

    Estrutura da aplicação - Componente: Post
    Nessa aula vamos estruturar o conteúdo do componente Post, nos baseando no conteúdo que podemos encontrar no Layout do Figma.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
-import { Post } from './Post';
+import { Post } from './components/Post';
 
 import styles from './App.module.css';
 
@@ -13,7 +13,7 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        
+
         <main>
           <Post
           author="John Doe"
