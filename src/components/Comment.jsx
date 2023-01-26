e25773a06ca8ec9e8149431891d31f4e57edc578
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-25 22:05:24 -0300

    Estrutura da aplicação - Estilizações do Comentário
    Agora que terminamos a estrutura dos comentários, precisamos estilizá-los seguindo o Layout no Figma, e é justamente isso que faremos nessa aula.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -10,17 +10,17 @@ export function Comment() {
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
-            <div className={styles.athorAndTime}>
+            <div className={styles.authorAndTime}>
               <strong>Nome do Usuário</strong>
               <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>Muito bom, parabéns!! 👏👏</p>
         </div>
 
         <footer>
