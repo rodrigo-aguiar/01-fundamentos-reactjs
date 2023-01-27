233294cf8e84104d7d470d71d4e090d810b5517a
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-26 22:17:24 -0300

    Os motores do React - Propriedades do Post
    Na última aula nós fizemos a iteração de um array de posts. Já nessa aula vamos aprender a como utilizar essa iteração para enviar os dados de cada post de nosso array para componente Post para que ele possa ser renderizado em tela dinâmicamente.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,51 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormated = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-        <Avatar src="https://github.com/rodrigo-aguiar.png" />
+        <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Nome do Usuário</strong>
-            <span>Cargo do usuário</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        <time
+          title={publishedDateFormated}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href='#'>jane.design/doctorcare</a></p>
-        <p>
-          <a href='#'>#novoprojeto</a>{' '}
-          <a href='#'>#nlw</a>{' '}
-          <a href='#'>#rocketseat </a>{' '}
-        </p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          }
+          
+          if (line.type === 'link') {
+            return <p><a href='#'>{line.content}</a></p>;
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
