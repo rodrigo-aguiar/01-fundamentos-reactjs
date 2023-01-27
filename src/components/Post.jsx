304b2ddaf07ba24ab49e23209d63a62b06d5170a
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-26 23:12:39 -0300

    Os motores do React - Entendendo a key
    Nessa aula vamos entender a propriedade key do React, que vem de um conceito importante do React, chamado Listas e chaves.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -59,11 +59,11 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           }
           
           if (line.type === 'link') {
-            return <p><a href='#'>{line.content}</a></p>;
+            return <p key={line.content}><a href='#'>{line.content}</a></p>;
           }
         })}
       </div>
@@ -85,7 +85,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment}/>;
+          return <Comment key={comment} content={comment}/>;
         })}
       </div>
     </article>
